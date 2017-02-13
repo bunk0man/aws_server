cd7064feb408352af48bc8a147ba5e656750425b
bunk0man <thomasckik@gmail.com>
2017-02-13 21:58:24 +0100

    json added

--- test_server.rb
+++ test_server.rb
@@ -1,6 +1,10 @@
 require 'aws-sdk'
+require 'json'
 
 Aws.config.update({
   region: 'us-west-2',
   credentials: Aws::Credentials.new('akid', 'secret')
 })
+# how it should be used?
+creds = JSON.load(File.read('secrets.json'))
+Aws.config[:credentials] = Aws::Credentials.new(creds['AccessKeyId'], creds['SecretAccessKey'])

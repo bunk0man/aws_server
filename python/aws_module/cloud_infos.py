f4069c5cd5000ae214ae5420f6871f8067ee318c
bunk0man <thomasckik@gmail.com>
2017-03-12 22:38:39 +0100

    creating class

---
+++ python/aws_module/cloud_infos.py
@@ -0,0 +1,19 @@
+import boto3
+import yaml
+
+
+class AwsHandler(object):
+    def __init__(self):
+        pass
+
+    def list_instances(self):
+        pass
+
+    def create_instances(self):
+        pass
+
+    def stop_instances(self):
+        pass
+
+    def start_instances(self):
+        pass
